842d10b3c3d9afdd6d582b04a207e7fc593a56e9
Peter Gawthrop <gawthrop@users.sourceforge.net>
1996-08-10 11:49:39 +0000

    Fixed multi-input/output problem

--- mttroot/mtt/bin/trans/m/dm2ir.m
+++ mttroot/mtt/bin/trans/m/dm2ir.m
@@ -1,4 +1,4 @@
-function [t,ir] = dm2ir(A,B,C,D,E,tmax,tstep);
+function [T,ir] = dm2ir(A,B,C,D,E,tmax,tstep);
 
 % Descriptor matrix to impulse response.
 % NB At the moment - this assumes that E is unity .....
@@ -8,8 +8,21 @@ function [t,ir] = dm2ir(A,B,C,D,E,tmax,tstep);
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% $Id$
 % %% $Log$
+% %% Revision 1.1  1996/08/10 10:26:00  peter
+% %% Initial revision
+% %%
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-t=[0:tstep:tmax]';
-ir = C*exp(A*t)*B;
+T=[0:tstep:tmax]';
+N = length(T);
+[Ny,Nu] = size(D);
+NN=Ny*Nu;
+
+ir = zeros(N,NN);
+i = 0;
+for t = T'
+  i=i+1;
+  ir(i,:) = reshape(C*exp(A*t)*B, 1,NN);
+end;
+

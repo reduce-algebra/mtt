2213265b8f0f551e1aac7bb977aa9c9851055631
Peter Gawthrop <gawthrop@users.sourceforge.net>
1996-08-10 14:16:04 +0000

    Now has T vector input - it was tmax & tstep

--- mttroot/mtt/bin/trans/m/dm2ir.m
+++ mttroot/mtt/bin/trans/m/dm2ir.m
@@ -1,21 +1,30 @@
-function [T,ir] = dm2ir(A,B,C,D,E,tmax,tstep);
-
+function ir = dm2ir(A,B,C,D,E,T);
+% ir = dm2ir(A,B,C,D,E,T);
 % Descriptor matrix to impulse response.
 % NB At the moment - this assumes that E is unity .....
+% A,B,C,D,E - descriptor matrices
+% T vector of time points
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% Version control history
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% $Id$
 % %% $Log$
+% %% Revision 1.2  1996/08/10 11:49:39  peter
+% %% Fixed multi-input/output problem
+% %%
 % %% Revision 1.1  1996/08/10 10:26:00  peter
 % %% Initial revision
 % %%
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-T=[0:tstep:tmax]';
-N = length(T);
+[N,M] = size(T);
+if M>N
+  T = T';
+  N = M;
+end;
+
 [Ny,Nu] = size(D);
 NN=Ny*Nu;
 

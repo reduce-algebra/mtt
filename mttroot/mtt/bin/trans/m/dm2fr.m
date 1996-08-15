0d46154a7e1637c4b59d95d64ada3fd5b0f90407
Peter Gawthrop <gawthrop@users.sourceforge.net>
1996-08-15 10:24:28 +0000

    Includes u0 argument.

--- mttroot/mtt/bin/trans/m/dm2fr.m
+++ mttroot/mtt/bin/trans/m/dm2fr.m
@@ -1,26 +1,42 @@
-function  fr = dm2fr(A,B,C,D,E,W)
-% fr = dm2fr(A,B,C,D,E,W)
+function  fr = dm2fr(A,B,C,D,E,W,u0)
+% fr = dm2fr(A,B,C,D,E,W,u0)
 % Descriptor matrix to frequency response.
 % A,B,C,D,E - descriptor matrices
 % W vector of frequency points
-
+% u0 input gain vector: u = u0*unit phasor
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% Version control history
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% $Id$
 % %% $Log$
+% %% Revision 1.1  1996/08/10 14:11:28  peter
+% %% Initial revision
+% %%
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-N = length(W);
 [Ny,Nu] = size(D);
-NN=Ny*Nu;
+[Ny,Nx] = size(C);
+N = length(W);
 
-fr = zeros(N,NN);
+if nargin<7
+  u0 = zeros(Nu,1);
+  u0(1) = 1;
+end;
+
+fr = zeros(N,Ny);
 i = 0;
 for w = W
   i = i+1;
   FR = C*( (E*j*w - A) \ B ) + D;
-  fr(i,:) = reshape(FR,1,NN);
+  fr(i,:) = FR';
 end;
+
+
+
+
+
+
+
+
 

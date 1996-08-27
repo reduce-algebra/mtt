2211ea99f223a7789775682a452d8c7f65d19972
Peter Gawthrop <gawthrop@users.sourceforge.net>
1996-08-27 08:04:52 +0000

    Handles complex components and repetative components.

--- mttroot/mtt/bin/trans/m/cbg2ese.m
+++ mttroot/mtt/bin/trans/m/cbg2ese.m
@@ -1,4 +1,6 @@
-function structure = cbg2ese(system_name, system_type, full_name, infofile)
+function structure = cbg2ese(system_name, system_type, full_name, ...
+    repetition,...
+    structure, infofile)
 % 
 %     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 %     %%%%% Model Transformation Tools %%%%%
@@ -6,12 +8,16 @@ function structure = cbg2ese(system_name, system_type, full_name, infofile)
 % 
 % Matlab function  cbg2ese.m
 % Acausal bond graph to causal bond graph: mfile format
+% Structure matrix [states,nonstates,inputs,outputs,zero_outputs]
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% Version control history
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% $Id$
 % %% $Log$
+% %% Revision 1.5  1996/08/24  15:02:23  peter
+% %% Writes `END;' to keep reduce happy.
+% %%
 % %% Revision 1.4  1996/08/19 09:03:41  peter
 % %% Handles repeating components.
 % %%
@@ -26,6 +32,7 @@ function structure = cbg2ese(system_name, system_type, full_name, infofile)
 % %%
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+system_name, system_type, full_name,
 
 pc = '%';
 if nargin<3
@@ -44,7 +51,6 @@ else
 end;
 
 full_name_type = [full_name, '_', system_type];
-ese_name = [system_name, '_ese.r'];
 cbg_name = [full_name_type, '_cbg'];
 abg_name = [system_type, '_abg'];
 cmp_name = [system_type, '_cmp'];
@@ -54,6 +60,7 @@ if exist(cbg_name)~=2
 end;
 
 % Setup file
+ese_name = sprintf('%s_%1.0f_ese.r', full_name, repetition);
 filenum = fopen(ese_name, 'w');
 
 % Evaluate the system function to get the bonds
@@ -67,11 +74,7 @@ filenum = fopen(ese_name, 'w');
 
   % Find number of components
   [n_components,columns] = size(components);
-  
-  %Structure matrix [states,nonstates,inputs,outputs,zero_outputs]
-  structure = zeros(1,5);
-
-
+  n_components = n_components
   for i = 1:n_components
     comp = nozeros(components(i,:));
     bond_list = abs(comp);
@@ -86,8 +89,11 @@ filenum = fopen(ese_name, 'w');
     if strcmp(comp_type,'1')
       comp_type = 'one';
     end;
-
-    ports = length(bond_list);
+    
+    comp_type = comp_type
+    ports = length(bond_list)
+    repetitions = repetitions
+    
     if repetitions>1
       port_pairs = ports/2;
       if round(port_pairs)~=port_pairs;
@@ -104,6 +110,7 @@ filenum = fopen(ese_name, 'w');
     end;
     
     for k = 1:repetitions
+      
       if repetitions>1
 	if k==1
 	  bond_list(1:2:ports-1) = odd_bonds;
@@ -121,18 +128,48 @@ filenum = fopen(ese_name, 'w');
       end;
 	
       % Invoke the appropriate equation-generating procedure
+      name_r = sprintf('%s_%1.0f', full_name, repetition);
       eqn_name = [comp_type, '_eqn']
       if exist(eqn_name)~=2 % Try a compound component
-	cbg2ese(comp_name, comp_type, full_name, infofile);
+	disp('---PUSH---');
+	structure = cbg2ese(comp_name, comp_type, full_name, k, ...
+	    structure,  infofile);
+	% Link up the bonds
+	name_k = sprintf('%s_%1.0f', full_name, k);
+	name_comp_name = sprintf('%s_%s_%1.0f', full_name, comp_name, k);
+	for port_number=1:length(bond_list)
+
+	  % Effort
+	  if comp_bonds(port_number,1)==1 % Source
+	     fprintf(filenum, '%s_MTT_inport_%1.0f := %s;\n', ...
+		 name_comp_name, port_number, varname(name_r, ...
+		 bond_list(port_number),1));
+	   else % sensor
+	     	 fprintf(filenum, '%s := %s_MTT_outport_%1.0f;\n', ...
+		 varname(name_r, ...
+		 bond_list(port_number),1), name_comp_name, port_number);
+	  end;
+	  % flow
+	  if comp_bonds(port_number,2)==-1 % Source
+	     fprintf(filenum, '%s_MTT_inport_%1.0f := %s;\n', ...
+		 name_comp_name, port_number, varname(name_r, ...
+		 bond_list(port_number),-1));
+	   else % sensor
+	     	 fprintf(filenum, '%s := %s_MTT_outport_%1.0f;\n', ...
+		 varname(name_r, ...
+		 bond_list(port_number),-1), name_comp_name, port_number);
+	   end;	
+	 end;
+	
+	disp('---POP---');
       else % its a simple component
 	eval(['structure = ', ...
 	      eqn_name, ...
-	      '(bond_list,comp_bonds,direction,cr,args,structure,filenum);' ]);
+	      '(name_r,bond_list,comp_bonds,direction,cr,args,structure,filenum);' ]);
       end;
     end;
   end;
 
-  fprintf(filenum, 'END;');
   fclose(filenum);
 
 

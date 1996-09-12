21dd40df50ec4df2b10012907f64d72f7ca90344
Peter Gawthrop <gawthrop@users.sourceforge.net>
1996-09-12 16:40:45 +0000

    Now does a default (when no constitutive relationship) with a unit constitutive relationship on each port.

--- mttroot/mtt/lib/comp/simple/R_eqn.m
+++ mttroot/mtt/lib/comp/simple/R_eqn.m
@@ -15,6 +15,9 @@ function  structure = R_eqn(name,bond_number,bonds,direction,cr,args, ...
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% $Id$
 % %% $Log$
+% %% Revision 1.2  1996/09/12 13:12:04  peter
+% %% Removed an fprintf that was a mistake.
+% %%
 % %% Revision 1.1  1996/09/12 11:00:56  peter
 % %% Initial revision
 % %%
@@ -35,9 +38,18 @@ end;
 for outport = 1:ports
   outcause = -bonds(outport,1);
   outnumber = bond_number(outport);
-  incause = bonds(:,1);
+  if length(cr)>0
+    incause = bonds(:,1);
+    inports = 1:ports;
+    inbonds = bond_number;
+  else % default if no cr: unit cr on each port
+    incause = bonds(outport,1);
+    inport = outport;
+    inbond = outnumber;
+  end;
+  
   eqn =  equation(name,cr,args,outnumber,outcause,outport, ...
-                               bond_number,incause,1:ports);
+                               inbond,incause,inport);
   fprintf(eqnfile, '%s',eqn);
 end;
 

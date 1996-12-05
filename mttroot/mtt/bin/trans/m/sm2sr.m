fad199ef8744f95dfe3184bca166f6762b6ebac4
Peter Gawthrop <gawthrop@users.sourceforge.net>
1996-12-05 10:38:36 +0000

    Decided that integrating the IR is a bad idea. So now explicitly computes the SR using matrix exponential -- much more accurate.

--- mttroot/mtt/bin/trans/m/sm2sr.m
+++ mttroot/mtt/bin/trans/m/sm2sr.m
@@ -16,6 +16,9 @@ function [Y,X] = sm2sr(A,B,C,D,T,u0,x0);
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% $Id$
 % %% $Log$
+% %% Revision 1.4  1996/12/05 10:18:52  peterg
+% %% Saved (quite old) version which integrates IR to give SR
+% %%
 % %% Revision 1.3  1996/10/27  10:39:04  peterg
 % %% Only compute matrix exponential once.
 % %%
@@ -48,45 +51,45 @@ end;
 
 
 
-% $$$ one = eye(Nx);
-% $$$ 
-% $$$ Y = zeros(N,Ny);
-% $$$ X = zeros(N,Nx);
-% $$$ 
-% $$$ dt = T(2)-T(1);% Assumes fixed interval
-% $$$ expAdt = expm(A*dt); % Compute matrix exponential
-% $$$ i = 0;
-% $$$ expAt = one;
-% $$$ 
-% $$$ for t = T'
-% $$$   i=i+1;
-% $$$   if Nx>0
-% $$$     x = ( A\(expAt-one) )*B*u0 + expAt*x0;
-% $$$     expAt = expAt+expAdt;
-% $$$     X(i,:) = x';
-% $$$     if Ny>0
-% $$$       y = C*x + D*u0;
-% $$$       Y(i,:) = y';
-% $$$     end;
-% $$$   elseif Ny>0
-% $$$     y = D*u0;
-% $$$     Y(i,:) = y';
-% $$$   end;
-% $$$ end;
+one = eye(Nx);
 
-% Compute the impulse response
-[Y,X] = sm2ir(A,B,C,D,T,u0,x0);
+Y = zeros(N,Ny);
+X = zeros(N,Nx);
 
-% Assume fixed sample interval
-dT = T(2)-T(1);
+dt = T(2)-T(1);% Assumes fixed interval
+expAdt = expm(A*dt); % Compute matrix exponential
+i = 0;
+expAt = one;
 
-% Do an Euler integration on it
- Y = mtt_euler(Y,dT);
- 
- if nargout>1
-   X = mtt_euler(X,dT);
- end;
- 
+for t = T'
+  i=i+1;
+  if Nx>0
+    x = ( A\(expAt-one) )*B*u0 + expAt*x0;
+    expAt = expAt*expAdt;
+    X(i,:) = x';
+    if Ny>0
+      y = C*x + D*u0;
+      Y(i,:) = y';
+    end;
+  elseif Ny>0
+    y = D*u0;
+    Y(i,:) = y';
+  end;
+end;
+
+% $$$ % Compute the impulse response
+% $$$ [Y,X] = sm2ir(A,B,C,D,T,u0,x0);
+% $$$ 
+% $$$ % Assume fixed sample interval
+% $$$ dT = T(2)-T(1);
+% $$$ 
+% $$$ % Do an Euler integration on it
+% $$$  Y = mtt_euler(Y,dT);
+% $$$  
+% $$$  if nargout>1
+% $$$    X = mtt_euler(X,dT);
+% $$$  end;
+% $$$  
 
 
 

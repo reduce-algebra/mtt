b7bae799f0d662e0011bbe2d0a3fdcd9dddd69c7
Peter Gawthrop <gawthrop@users.sourceforge.net>
1997-04-09 13:00:28 +0000

    *** empty log message ***

--- mttroot/mtt/lib/comp/simple/C_eqn.m
+++ mttroot/mtt/lib/comp/simple/C_eqn.m
@@ -1,7 +1,14 @@
-function structure =  C_eqn(bond_number,bonds,direction,cr,args, ...
-                            structure,eqnfile);
-% C_eqn: equation generation for C component
-
+function structure =  C_eqn(name,bond_number,bonds,direction,cr,args, ...
+    structure,eqnfile);
+% C_eqn - equation generation for C component
+% 
+%     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%     %%%%% Model Transformation Tools %%%%%
+%     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Matlab function  C_eqn
+% structure =  C_eqn(name,bond_number,bonds,direction,cr,args, ...
+%    structure,eqnfile);
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% Version control history
@@ -11,15 +18,19 @@ function structure =  C_eqn(bond_number,bonds,direction,cr,args, ...
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
+% Copyright (c) P.J. Gawthrop, 1996.
+
 			
-if nargin<7
+if nargin<8
   eqnfile = 'stdout';
 end;
 
 % Unicausal version
 CorI = 1;
-structure = cieqn(bond_number,bonds,direction,cr,args, structure, ...
+structure = cieqn(name,bond_number,bonds,direction,cr,args, structure, ...
                  CorI, eqnfile);
+
+
 
 
 

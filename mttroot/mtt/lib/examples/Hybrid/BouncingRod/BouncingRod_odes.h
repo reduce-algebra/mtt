71c1fc9a79548de8a59d531a7bc4a71bfca8d3c8
Peter Gawthrop <gawthrop@users.sourceforge.net>
1997-07-06 16:20:44 +0000

    Initial revision

---
+++ mttroot/mtt/lib/examples/Hybrid/BouncingRod/BouncingRod_odes.h
@@ -0,0 +1,7 @@
+/*
+%% Parameter file for system BouncingRod (BouncingRod_odes.h)
+%% This file provides the params for simulation:
+*/
+#define DT 0.1 /* Time step (for printing) */
+#define LAST 99.9 /* Last time */
+#define STEPFACTOR 1000 /* Integration steps per time step */

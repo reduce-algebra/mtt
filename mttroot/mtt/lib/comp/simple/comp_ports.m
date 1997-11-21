d5fd0555a8c1dcf6ec2e22cf17097379c4b23ff6
Peter Gawthrop <gawthrop@users.sourceforge.net>
1997-11-21 11:32:57 +0000

    N ports numbered 1..N

--- mttroot/mtt/lib/comp/simple/comp_ports.m
+++ mttroot/mtt/lib/comp/simple/comp_ports.m
@@ -1,4 +1,4 @@
-function ports = comp_ports(comp_type)
+function ports = comp_ports(comp_type,N)
 %
 %     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 %     %%%%% Model Transformation Tools %%%%%
@@ -14,6 +14,9 @@ function ports = comp_ports(comp_type)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% $Id$
 % %% $Log$
+% %% Revision 1.2  1997/08/28  08:08:24  peterg
+% %% Added RS component to the two-port list
+% %%
 % %% Revision 1.1  1997/08/02 19:35:47  peterg
 % %% Initial revision
 % %%
@@ -21,9 +24,9 @@ function ports = comp_ports(comp_type)
 
 
 junctions = '-zero-one-';
-one_ports = '-SS-R-C-I-';
+one_ports = '-SS-';
 two_ports = '-TF-GY-AE-AF-FMR-RS-';
-
+N_ports   = '-R-C-I-';
 comp_type = ['-', comp_type, '-'];
 
 if length(findstr(comp_type,junctions))==1
@@ -34,8 +37,20 @@ elseif length(findstr(comp_type,two_ports))==1
   ports = ['in';'out'];
 elseif length(findstr(comp_type,'[-EMTF-]'))==1
   ports = ['in';'out';'mod'];
+elseif length(findstr(comp_type,'[-ES-]'))==1
+  ports = ['e';'s'];
+elseif length(findstr(comp_type,N_ports))==1
+  if N==1
+    ports = ['in'];
+  elseif N==2
+    ports = ['in';'out'];
+  elseif N>2
+    ports = '1';
+    for i=2:N
+      ports = [ports; sprintf("%i",i)];
+    end;
+  end;
 end;
-
 
 
 

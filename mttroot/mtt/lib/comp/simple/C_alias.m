055003fa8878dd0aeeb8ec3ccd1c7b6bd0ff18e0
Peter Gawthrop <gawthrop@users.sourceforge.net>
1998-07-08 15:45:50 +0000

    out now aliases 2

--- mttroot/mtt/lib/comp/simple/C_alias.m
+++ mttroot/mtt/lib/comp/simple/C_alias.m
@@ -5,10 +5,13 @@ function alias = C_alias	# Port aliases for C
 ###############################################################
 ## $Id$
 ## $Log$
+## Revision 1.1  1998/07/02 14:49:14  peterg
+## Initial revision
+##
 ###############################################################
 
   alias.in     = '1';
-  alias.out    = '1';
+  alias.out    = '2';
 
 endfunction
 

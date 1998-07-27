404a275ecf0937657991a7cd0ee5fff199663587
Peter Gawthrop <gawthrop@users.sourceforge.net>
1998-07-27 17:34:59 +0000

    Sorted syntax errors

--- mttroot/mtt/bin/trans/lang_matrix.r
+++ mttroot/mtt/bin/trans/lang_matrix.r
@@ -13,6 +13,9 @@
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % $Id$
 % % $Log$
+% % Revision 1.3  1998/07/27 16:31:10  peterg
+% % Sorted out to work with mtt_r2m
+% %
 % % Revision 1.2  1998/05/23 15:00:27  peterg
 % % Removed the name = matrix statement - now done by sed.
 % %
@@ -25,14 +28,16 @@
 
 PROCEDURE Lang_Matrix();
 BEGIN
-  ON NERO;
-  IF MTT_Matrix_n>0 THEN
-    IF MTT_Matrix_m>0 THEN
-    BEGIN
     FOR i := 1:MTT_Matrix_n DO
-      FOR j := 1:MTT_Matrix_m DO 
-          GENTRAN mtt_matrix(i,j) ::=: mtt_matrix(i,j);
-    END;
+    BEGIN
+      IF MTT_Matrix_m>1 THEN
+      BEGIN
+        FOR j := 1:MTT_Matrix_m DO 
+            GENTRAN mtt_matrix(i,j) ::=: mtt_matrix(i,j);
+      END
+      ELSE
+        GENTRAN mtt_matrix(i) ::=: mtt_matrix(i,1);
+    END
 END;
 
 END;;

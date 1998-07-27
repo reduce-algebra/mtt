1d9d2e9113e1428ba0cf856f9e44c81961556f70
Peter Gawthrop <gawthrop@users.sourceforge.net>
1998-07-27 20:19:33 +0000

    Stopped it trying to fix its own causality.

--- mttroot/mtt/lib/comp/simple/C_cause.m
+++ mttroot/mtt/lib/comp/simple/C_cause.m
@@ -20,6 +20,9 @@ function [bonds,status] = C_cause(bonds);
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %% $Id$
 % %% $Log$
+% %% Revision 1.1  1996/11/01 12:35:36  peterg
+% %% Initial revision
+% %%
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -28,6 +31,39 @@ function [bonds,status] = C_cause(bonds);
 % Unicausal multiport C component
 % Same causal pattern as R component
 
-[bonds,status] = R_cause(bonds);
+% [bonds,status] = R_cause(bonds);
+
+% Set causality to preferred if not already set.
+% Find the number of ports
+[ports,junk] = size(bonds);
+
+% default undercausal status
+statuses = -ones(ports,1);
+
+% Force unicausality but DONT set if not already set
+preferred = -1;
+for i = 1:ports
+  if (bonds(i,1)~=0)&(bonds(i,2)~=0) % Both bonds set
+    statuses(i) = bonds(i,1)~=bonds(i,2);
+  elseif bonds(i,2)~=0               % Bond 1 set
+    bonds(i,1) = bonds(i,2);
+    statuses(i) = 0;
+  elseif bonds(i,1)~=0               % Bond 2 set
+    bonds(i,2) = bonds(i,1);
+    statuses(i) = 0;
+  else				# Don't set
+#    bonds(i,1) = preferred;
+#    bonds(i,2) = preferred;
+#    statuses(i) = 0;
+  end;
+end;
+
+if max(statuses)==1
+  status = 1;
+elseif min(statuses)==-1
+  status = -1;
+else
+  status = 0;
+end;
 
 

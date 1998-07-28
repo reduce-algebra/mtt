f04d16370a322c318bcce3bc630b69430919fa9e
Peter Gawthrop <gawthrop@users.sourceforge.net>
1998-07-28 19:08:22 +0000

    Some vector aliases added.

--- mttroot/mtt/lib/comp/simple/SS_alias.m
+++ mttroot/mtt/lib/comp/simple/SS_alias.m
@@ -5,10 +5,28 @@ function alias = SS_alias	# Port aliases for SS
 ###############################################################
 ## $Id$
 ## $Log$
+## Revision 1.1  1998/07/02 14:37:42  peterg
+## Initial revision
+##
 ###############################################################
 
-  alias.in   = '1';
-  alias.out  = '1';
+  alias.in   = "1";
+  alias.out  = "1";
+
+  alias.in1   = "1";
+  alias.out1  = "1";
+
+  alias.in2   = "1,2";
+  alias.out2  = "1,2";
+
+  alias.in3   = "1,2,3";
+  alias.out3  = "1,2,3";
+
+  alias.in4   = "1,2,3,4";
+  alias.out4  = "1,2,3,4";
+
+  alias.in5   = "1,2,3,4,5";
+  alias.out5  = "1,2,3,4,5";
 
 endfunction
 

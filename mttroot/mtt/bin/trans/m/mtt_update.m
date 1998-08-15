c7c2296a1a1ca8c052e54684432de6dd87da2c86
Peter Gawthrop <gawthrop@users.sourceforge.net>
1998-08-15 13:48:11 +0000

    New way of computing Euler

--- mttroot/mtt/bin/trans/m/mtt_update.m
+++ mttroot/mtt/bin/trans/m/mtt_update.m
@@ -1,5 +1,12 @@
-function [x] = mtt_update(dx,x,dt,Nx,METHOD);
+function [x] = mtt_update(dx,x,AAx,AA,dt,STEPFACTOR,Nx,METHOD);
 
-  x = x + dx*dt;
+  if (METHOD==1)		#Euler
+    ddt = dt/STEPFACTOR;
+      x = x + dx*ddt;
+  elseif ((METHOD==2)||(METHOD==3))#Linear implicit or Implicit
+    x = AA\(AAx + dx*dt);
+  else
+    error("Method %i is not defined", METHOD);
+  endif;
 
 endfunction;

45dbf70680c14292c0c1c4eb77e999d56fbfcc90
Peter Gawthrop <gawthrop@users.sourceforge.net>
1998-08-26 19:07:17 +0000

    Euler or Implicit only

--- mttroot/mtt/bin/trans/m/mtt_update.m
+++ mttroot/mtt/bin/trans/m/mtt_update.m
@@ -1,9 +1,10 @@
-function [x] = mtt_update(dx,x,AAx,AA,dt,STEPFACTOR,Nx,METHOD);
+function [x] = mtt_update(dx,x,dt,STEPFACTOR,METHOD,name);
 
   if (METHOD==1)		#Euler
     ddt = dt/STEPFACTOR;
-      x = x + dx*ddt;
-  elseif ((METHOD==2)||(METHOD==3))#Linear implicit or Implicit
+    x = x + dx*ddt;
+  elseif (METHOD==2)		#Linear implicit or Implicit
+    eval("[AA,AAx] = ",name,"_smx;");
     x = AA\(AAx + dx*dt);
   else
     error("Method %i is not defined", METHOD);

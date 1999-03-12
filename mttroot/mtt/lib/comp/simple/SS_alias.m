24ed761149617c948eaa2f159a9f6a45bbc3b65c
Peter Gawthrop <gawthrop@users.sourceforge.net>
1999-03-12 01:12:09 +0000

    Now does up to 10 ports

--- mttroot/mtt/lib/comp/simple/SS_alias.m
+++ mttroot/mtt/lib/comp/simple/SS_alias.m
@@ -5,6 +5,9 @@ function alias = SS_alias	# Port aliases for SS
 ###############################################################
 ## $Id$
 ## $Log$
+## Revision 1.2  1998/07/28 19:08:22  peterg
+## Some vector aliases added.
+##
 ## Revision 1.1  1998/07/02 14:37:42  peterg
 ## Initial revision
 ##
@@ -13,21 +16,13 @@ function alias = SS_alias	# Port aliases for SS
   alias.in   = "1";
   alias.out  = "1";
 
-  alias.in1   = "1";
-  alias.out1  = "1";
-
-  alias.in2   = "1,2";
-  alias.out2  = "1,2";
-
-  alias.in3   = "1,2,3";
-  alias.out3  = "1,2,3";
-
-  alias.in4   = "1,2,3,4";
-  alias.out4  = "1,2,3,4";
-
-  alias.in5   = "1,2,3,4,5";
-  alias.out5  = "1,2,3,4,5";
-
+  MAX = 10;
+  value = "1";
+  for i=1:MAX
+    eval(sprintf("alias.in%i = \"%s\";", i, value));
+    eval(sprintf("alias.out%i = \"%s\";", i, value));
+    value = sprintf("%s,%i",value,i+1);
+  endfor;
 endfunction
 
 

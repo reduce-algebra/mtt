48b97548efb3c7789a83cee2a82a0a436cd92026
Peter Gawthrop <gawthrop@users.sourceforge.net>
1999-03-15 23:05:29 +0000

    A complete rewrite - now puts into a Global matrix MTT_data

--- mttroot/mtt/bin/trans/m/mtt_write.m
+++ mttroot/mtt/bin/trans/m/mtt_write.m
@@ -5,19 +5,30 @@ function mtt_write(t,x,y,nx,ny);
 ###############################################################
 ## $Id$
 ## $Log$
+## Revision 1.3  1999/03/15 21:57:00  peterg
+## Removed the # symbol
+##
 ###############################################################
 
+global MTT_data
+  if t==0.0
+    MTT_data=[];
+  endif
 
-  printf("%e ", t);
-  for i=1:ny
-    printf("%e ", y(i));
-  endfor
+MTT_data = [MTT_data; t,y',t,x'];
 
-  printf("%e ", t);
-  for i=1:nx
-    printf("%e ", x(i));
-  endfor
-  printf("\n");
+
+
+#   printf("%e ", t);
+#   for i=1:ny
+#     printf("%e ", y(i));
+#   endfor
+
+#   printf("%e ", t);
+#   for i=1:nx
+#     printf("%e ", x(i));
+#   endfor
+#   printf("\n");
 
 endfunction
 

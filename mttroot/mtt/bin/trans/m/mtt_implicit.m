64ad17c8ebb4e6af20095e30930918333e234fd0
Peter Gawthrop <gawthrop@users.sourceforge.net>
1999-04-20 00:58:22 +0000

    Set open-switch states to zero

--- mttroot/mtt/bin/trans/m/mtt_implicit.m
+++ mttroot/mtt/bin/trans/m/mtt_implicit.m
@@ -5,11 +5,16 @@ function [x] = mtt_implicit(x,dx,AA,AAx,dt,Nx,open); # Implicit update
 ## ###############################################################
 ## ## $Id$
 ## ## $Log$
+## ## Revision 1.1  1999/04/02 06:23:20  peterg
+## ## Initial revision
+## ##
 ## ###############################################################
 
 ## Copyright (C) 1999 by P.J. Gawthrop
 
+  I_open = nozeros(open.*[1:Nx]'); # Indices of open switches
+  x(I_open) = 0.0;		# Open switches have zero state
   I = nozeros(!open.*[1:Nx]');	# Indices of states to update
-  x(I) = AA(I,I)\(AAx(I) + dx(I)*dt);	# Implicit update (exept open switches);
+  x(I) = AA(I,I)\(AAx(I) + dx(I)*dt);	# Implicit update (except open switches);
 
 endfunction

c443b023ad7f898fec83840474b26e5cbbd44b27
Peter Gawthrop <gawthrop@users.sourceforge.net>
2000-05-19 19:54:50 +0000

    Initial revision

---
+++ mttroot/mtt/lib/comp/compound/Chemical/Rate/Rate_cr.r
@@ -0,0 +1,21 @@
+%% CR file for rate-of reaction component RATE
+
+OPERATOR Rate;
+
+% Reaction rate
+FOR ALL k,q,h,n,Conc,Temp LET
+    Rate(k,q,h,n,flow,1,
+         Conc,effort,1,
+	 Temp,effort,2
+	 ) 
+	 = k*Conc^n*e^(-q/Temp);
+
+% Heat
+FOR ALL k,q,h,n,Conc,Temp LET
+    Rate(k,q,h,n,flow,2,
+         Conc,effort,1,
+	 Temp,effort,2
+	 ) 
+	 = k*Conc^n*h*e^(-q/Temp);
+
+END;

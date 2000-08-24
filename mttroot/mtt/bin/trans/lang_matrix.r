1254fc03bd12c88adef060bd3397d4b41d051e20
Peter Gawthrop <gawthrop@users.sourceforge.net>
2000-08-24 17:12:01 +0000

    Now optimises using SCOPE

--- mttroot/mtt/bin/trans/lang_matrix.r
+++ mttroot/mtt/bin/trans/lang_matrix.r
@@ -13,6 +13,9 @@
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % $Id$
 % % $Log$
+% % Revision 1.4  1998/07/27 17:34:59  peterg
+% % Sorted syntax errors
+% %
 % % Revision 1.3  1998/07/27 16:31:10  peterg
 % % Sorted out to work with mtt_r2m
 % %
@@ -28,15 +31,21 @@
 
 PROCEDURE Lang_Matrix();
 BEGIN
-    FOR i := 1:MTT_Matrix_n DO
+    FOR row := 1:MTT_Matrix_n DO
     BEGIN
       IF MTT_Matrix_m>1 THEN
       BEGIN
-        FOR j := 1:MTT_Matrix_m DO 
-            GENTRAN mtt_matrix(i,j) ::=: mtt_matrix(i,j);
+        FOR col := 1:MTT_Matrix_m DO
+            BEGIN
+              INAME(mtt_t);
+              GENTRAN mtt_matrix(row,col) ::=: mtt_matrix(row,col);
+            END;
       END
       ELSE
-        GENTRAN mtt_matrix(i) ::=: mtt_matrix(i,1);
+        BEGIN
+          INAME(mtt_o);
+          GENTRAN mtt_matrix(row) ::=: mtt_matrix(row,1);
+        END;
     END
 END;
 

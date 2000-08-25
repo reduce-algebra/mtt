a4e2f78c4c72e03748e30dbba94c80669a3cb5f9
Peter Gawthrop <gawthrop@users.sourceforge.net>
2000-08-25 09:23:40 +0000

    Made both names the same!

--- mttroot/mtt/bin/trans/lang_matrix.r
+++ mttroot/mtt/bin/trans/lang_matrix.r
@@ -13,6 +13,9 @@
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % $Id$
 % % $Log$
+% % Revision 1.5  2000/08/24 17:12:01  peterg
+% % Now optimises using SCOPE
+% %
 % % Revision 1.4  1998/07/27 17:34:59  peterg
 % % Sorted syntax errors
 % %
@@ -37,13 +40,13 @@ BEGIN
       BEGIN
         FOR col := 1:MTT_Matrix_m DO
             BEGIN
-              INAME(mtt_t);
+              INAME(mtt_o); % Set temp name
               GENTRAN mtt_matrix(row,col) ::=: mtt_matrix(row,col);
             END;
       END
       ELSE
         BEGIN
-          INAME(mtt_o);
+          INAME(mtt_o); % Set temp name
           GENTRAN mtt_matrix(row) ::=: mtt_matrix(row,1);
         END;
     END

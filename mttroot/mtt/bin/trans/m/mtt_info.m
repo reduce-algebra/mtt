e2cb4e878a9ab07d2b323d562654ad01baa163b9
Peter Gawthrop <gawthrop@users.sourceforge.net>
2000-11-12 17:10:51 +0000

    Close file if it is opened Reformated  octave style

--- mttroot/mtt/bin/trans/m/mtt_info.m
+++ mttroot/mtt/bin/trans/m/mtt_info.m
@@ -1,21 +1,39 @@
 function mtt_info(info, infofile);
-% mtt_info(info, infofile);
+  ## mtt_info(info, infofile);
 
-% function [bonds, status] = abg2cbg(bonds,components,system_name,filename)
-% 
-%     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-%     %%%%% Model Transformation Tools %%%%%
-%     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  ## function [bonds, status] = abg2cbg(bonds,components,system_name,filename)
+  ## 
+  ##     ###################################### 
+  ##     ##### Model Transformation Tools #####
+  ##     ######################################
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% %% Version control history
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% %% $Id$
-% %% $Log$
-% %% Revision 1.1  1996/08/18  20:06:57  peter
-% %% Initial revision
-% %%
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  ## ###############################################################
+  ## ## Version control history
+  ## ###############################################################
+  ## ## $Id$
+  ## ## $Log$
+  ## ## Revision 1.2  1997/02/11 10:06:42  peterg
+  ## ## Removed a debugging line.
+  ## ##
+  ## ## Revision 1.1  1996/08/18  20:06:57  peter
+  ## ## Initial revision
+  ## ##
+  ## ###############################################################
 
-fprintf(infofile, 'INFORMATION: %s\n', info);
+  ## Set default file if it isn't there already
+  if nargin<2
+    infofile = fopen("mtt_info.txt","a");
+  end;
+
+  fprintf(infofile, "INFORMATION: %s\n", info);
+
+  if nargin<2
+    fclose(infofile);
+  end;
+
+endfunction
+
+
+
+
 

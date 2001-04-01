5119fb91c79efe6fda38426c338b0b33c11047b1
Geraint Paul Bevan <geraint@users.sourceforge.net>
2001-04-01 05:18:57 +0000

    Octave 2.0.16 compatibility. Removed static declarations - gave wrong results.

--- mttroot/mtt/lib/cc/mtt_implicit.cc
+++ mttroot/mtt/lib/cc/mtt_implicit.cc
@@ -15,18 +15,18 @@ DEFUN_DLD (mtt_implicit, args, ,
 	   "implicit integration method")
 {
 #ifdef OCTAVE_DEV
-  static ColumnVector  	x	= args(0).column_vector_value ();
-  static ColumnVector	dx	= args(1).column_vector_value ();
-  static Matrix		AA	= args(2).matrix_value ();
-  static ColumnVector	AAx	= args(3).column_vector_value ();
+  ColumnVector  	x	= args(0).column_vector_value ();
+  ColumnVector		dx	= args(1).column_vector_value ();
+  Matrix		AA	= args(2).matrix_value ();
+  ColumnVector		AAx	= args(3).column_vector_value ();
   const  double		t	= args(4).double_value ();
   const  int		Nx	= (int) (args(5).double_value ());
   const  ColumnVector	openx	= args(6).column_vector_value ();
 #else // !OCTAVE_DEV
-  static ColumnVector	x	= args(0).vector_value ();
-  static ColumnVector	dx	= args(1).vector_value ();
-  static Matrix		AA	= args(2).matrix_value ();
-  static ColumnVector	AAx	= args(3).vector_value ();
+  ColumnVector		x	= args(0).vector_value ();
+  ColumnVector		dx	= args(1).vector_value ();
+  Matrix		AA	= args(2).matrix_value ();
+  ColumnVector		AAx	= args(3).vector_value ();
   const  double		t	= args(4).double_value ();
   const  int		Nx	= (int) (args(5).double_value ());
   const  ColumnVector	openx	= args(6).vector_value ();
@@ -49,7 +49,15 @@ DEFUN_DLD (mtt_implicit, args, ,
 	}
     }
 
-  x = static_cast<ColumnVector> (xleftdiv (AA, static_cast<Matrix>(static_cast<ColumnVector>(AAx + dx * t))));
+#ifdef OCTAVE_DEV
+  x = static_cast<ColumnVector> (xleftdiv (AA, static_cast<Matrix>(AAx + dx * t)));
+#else // !OCTAVE_DEV
+  Matrix tmp = xleftdiv (AA, static_cast<Matrix>(static_cast<ColumnVector>(AAx + dx * t)));
+  for (row = 0; row < Nx; row++)
+    {
+      x (row) = tmp (row,0);
+    }
+#endif // OCTAVE_DEV
   
   for (row = 0; row < Nx; row++)
     {

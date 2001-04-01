78d6c44767011914444d79e0417332b050f98c6f
Geraint Paul Bevan <geraint@users.sourceforge.net>
2001-04-01 05:17:22 +0000

    Removed static declarations - gave wrong results with multiple calls of ode2odes.

--- mttroot/mtt/lib/cc/mtt_euler.cc
+++ mttroot/mtt/lib/cc/mtt_euler.cc
@@ -12,13 +12,13 @@ DEFUN_DLD (mtt_euler, args, ,
 	   "euler integration method")
 {
 #ifdef OCTAVE_DEV
-  static ColumnVector  	x	= args(0).column_vector_value ();
+  ColumnVector  	x	= args(0).column_vector_value ();
   const ColumnVector	dx	= args(1).column_vector_value ();
   const double		ddt	= args(2).double_value ();
   const int		Nx	= static_cast<int> (args(3).double_value ());
   const ColumnVector   	openx	= args(4).column_vector_value ();
 #else // !OCTAVE_DEV
-  static ColumnVector	x	= args(0).vector_value ();
+  ColumnVector		x	= args(0).vector_value ();
   const ColumnVector   	dx	= args(1).vector_value ();
   const double		ddt	= args(2).double_value ();
   const int		Nx	= static_cast<int> (args(3).double_value ());

d311af79b56341749515154a244694fd3b482316
Peter Gawthrop <gawthrop@users.sourceforge.net>
2001-05-08 15:18:12 +0000

    Added trig and hyperbolic functions to argument exclusion list

--- mttroot/mtt/bin/trans/m/figfig.m
+++ mttroot/mtt/bin/trans/m/figfig.m
@@ -1,17 +1,21 @@
-function figfig(filename,language)
-  ## Usage: figfig(filename[,language])
+function figfig(filename,language,boxed)
+  ## Usage: figfig(filename[,language,boxed])
   ## Puts octave figure into fig file (filename.fig)
   ## If second argument, converts to filename.language using fig2dev
   ## eg:
   ##    figfig("foo");
   ##    figfig("foo","eps");
   ##    figfig("foo","pdf");
+  ## Boxed=1 gives a box aroundd the figure
 
   ###############################################################
   ## Version control history
   ###############################################################
   ## $Id$
   ## $Log$
+  ## Revision 1.3  2001/04/10 12:54:50  gawthrop
+  ## Minor fixes for sensitivity versions
+  ##
   ## Revision 1.2  2000/12/27 16:06:02  peterg
   ## *** empty log message ***
   ##
@@ -19,6 +23,10 @@ function figfig(filename,language)
   ## Initial revision
   ###############################################################
 
+  if nargin<3
+    boxed=1
+  endif
+  
   figfilename = sprintf("%s.fig",filename);
   
   eval(sprintf("gset output \"%s\" ",figfilename));
@@ -29,11 +37,14 @@ function figfig(filename,language)
   gset output 
   replot;
 
-  ## Add a box - makes a visible bounding box
-  fid = fopen(figfilename,"a+");
-  fprintf(fid,"2 4 0 2 31 7 50 0 -1 0.000 0 0 7 0 0 5\n");
-  fprintf(fid,"\t9675 5310 9675 270 225 270 225 5310 9675 5310\n");
-  fclose(fid);
+
+  if boxed # Add a box - makes a visible bounding box
+    fid = fopen(figfilename,"a+");
+    fprintf(fid,"2 4 0 2 31 7 50 0 -1 0.000 0 0 7 0 0 5\n");
+    fprintf(fid,"\t9675 5310 9675 270 225 270 225 5310 9675 5310\n");
+    fclose(fid);
+  endif
+  
 
   if nargin>1			# Do a ps file
     psfilename = sprintf("%s.%s",filename,language);

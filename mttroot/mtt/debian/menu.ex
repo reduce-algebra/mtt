0c71ca8b37bc9d7a8c9da0c9e75e2719461efb19
Geraint Paul Bevan <geraint@users.sourceforge.net>
2002-04-28 02:54:55 +0000

    Intitial versions of debian package control files.

---
+++ mttroot/mtt/debian/menu.ex
@@ -0,0 +1,4 @@
+?package(mtt):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="mtt" command="/usr/bin/mtt"
+?package(mtt):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="xmtt" command="/usr/bin/xmtt"

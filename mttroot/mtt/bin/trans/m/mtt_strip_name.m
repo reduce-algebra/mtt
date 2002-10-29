b3357fd01227632e50dcfb13c4d1b3dc06236baa
Peter Gawthrop <gawthrop@users.sourceforge.net>
2002-10-29 23:44:57 +0000

    More informative error message

--- mttroot/mtt/bin/trans/m/mtt_strip_name.m
+++ mttroot/mtt/bin/trans/m/mtt_strip_name.m
@@ -4,6 +4,17 @@ function stripped_name = mtt_strip_name (name)
   ##
   ## Removes blanks and [] from a port name
 
+  if (length(name)==0)
+    error("Zero length port name found");
+  endif
+
+
+  [N,M] = size(name)
+  if N>1
+    error("Cannot resolve port names: redraw abg.fig");
+  endif
+  
+  
   stripped_name = deblank(name); # remove blanks
   stripped_name = stripped_name(2:length(stripped_name)-1);
   stripped_name = deblank(stripped_name); # remove blanks

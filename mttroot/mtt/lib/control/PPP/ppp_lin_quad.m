425309d6592a37658796bfb6694e3465746da9a9
Peter Gawthrop <gawthrop@users.sourceforge.net>
2002-12-11 12:41:46 +0000

    LQ version of linear PPP

---
+++ mttroot/mtt/lib/control/PPP/ppp_lin_quad.m
@@ -0,0 +1,18 @@
+function [k_x,k_w,K_x,K_w,Us0,J_uu,J_ux,J_uw,J_xx,J_xw,J_ww,y_u,A_u] = \
+      ppp_lin_quad (A,B,C,D,tau,Q,R)
+
+  ## usage:
+  ## [k_x,k_w,K_x,K_w,Us0,J_uu,J_ux,J_uw,J_xx,J_xw,J_ww,y_u,cond_uu] =
+  ## ppp_lin_quad (A,B,C,D,tau,Q,R)
+  ##
+  ## 
+
+  ## Steady-state Linear Quadratic solution
+  ## using Algebraic Riccati equation (ARE)
+  [P,A_u,A_w] = ppp_are (A,B,C,D,Q,R);
+
+  ## PPP solution
+  [k_x,k_w,K_x,K_w,Us0,J_uu,J_ux,J_uw,J_xx,J_xw,J_ww] = \
+      ppp_lin(A,B,C,D,A_u,A_w,tau,Q,R,P);
+
+endfunction

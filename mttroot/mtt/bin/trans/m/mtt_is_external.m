7951aac6fd51754f98bafe61bf145fdf75298d6e
Peter Gawthrop <gawthrop@users.sourceforge.net>
2003-03-25 11:50:54 +0000

    Added is_port -- but not used at the moment. Some explanation added.

--- mttroot/mtt/bin/trans/m/mtt_is_external.m
+++ mttroot/mtt/bin/trans/m/mtt_is_external.m
@@ -1,8 +1,12 @@
 function is_external = mtt_is_external (comp_type,outsig, insigs, is_port)
 
-  ## usage:  is_external = mtt_is_external (comp_type,outsig, insigs)
+  ## usage:  is_external = mtt_is_external (comp_type,outsig, insigs, is_port)
   ##
-  ## 
+  ## This is to determine if mtt_component_equation should look for
+  ## another equation. If is_external, the varable is available as a
+  ## system input and therfore requires no further equation.
+  ## I suspect that it needs more work.
+
 
   if nargin<4
     is_port = 0;
@@ -10,7 +14,9 @@ function is_external = mtt_is_external (comp_type,outsig, insigs, is_port)
 
   is_external = 0;			# Default
 
-  if strcmp(comp_type,"SS");
+  if is_port
+    is_external = 0;
+  elseif strcmp(comp_type,"SS");
     is_external = insigs(1,2)!=outsig(2);
   else
     is_external = 0;

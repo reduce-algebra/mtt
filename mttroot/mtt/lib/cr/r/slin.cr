4934eb051f068b29abed43d4c4940a120905ada0
Peter Gawthrop <gawthrop@users.sourceforge.net>
2003-05-16 11:18:03 +0000

    New COMPONENT argument.

--- mttroot/mtt/lib/cr/r/slin.cr
+++ mttroot/mtt/lib/cr/r/slin.cr
@@ -12,32 +12,32 @@ LET slin(gain_causality, gain, other_causality, 1, input, causality, 1)
 % Two port version for sC,sI,sR 
 
 % Port 1 - standard 
-FOR ALL gain_causality, gain, sgain, causality, input, sinput, other_causality
-LET slin(gain_causality, gain, sgain, other_causality, 1, 
+FOR ALL COMPONENT, gain_causality, gain, sgain, causality, input, sinput, other_causality
+LET slin(COMPONENT,gain_causality, gain, sgain, other_causality, 1, 
        input, causality, 1,
        sinput, causality, 2)
-         = lin(gain_causality, gain, other_causality, 1, 
+         = lin(COMPONENT,gain_causality, gain, other_causality, 1, 
                input, causality, 1); 
 
 
 % Port 2 - sensitivity
-FOR ALL gain_causality, gain, sgain, causality, input, sinput, other_causality
+FOR ALL COMPONENT, gain_causality, gain, sgain, causality, input, sinput, other_causality
 SUCH THAT sgain NEQ 0
-LET slin(gain_causality, gain, sgain, other_causality, 2, 
+LET slin(COMPONENT,gain_causality, gain, sgain, other_causality, 2, 
        input, causality, 1,
        sinput, causality, 2)
-         = lin(gain_causality, gain, other_causality, 1, 
+         = lin(COMPONENT,gain_causality, gain, other_causality, 1, 
                sinput, causality, 1)
-         + sgain*df(lin(gain_causality, gain, other_causality, 1, 
+         + sgain*df(lin(COMPONENT,gain_causality, gain, other_causality, 1, 
                input, causality, 1), gain); 
 
 %% Don't compute the derivative in this case
-FOR ALL gain_causality, gain, sgain, causality, input, sinput, other_causality
+FOR ALL COMPONENT, gain_causality, gain, sgain, causality, input, sinput, other_causality
 SUCH THAT sgain EQ 0
-LET slin(gain_causality, gain, sgain, other_causality, 2, 
+LET slin(COMPONENT,gain_causality, gain, sgain, other_causality, 2, 
        input, causality, 1,
        sinput, causality, 2)
-         = lin(gain_causality, gain, other_causality, 1, 
+         = lin(COMPONENT,gain_causality, gain, other_causality, 1, 
                sinput, causality, 1);
          
 
@@ -45,8 +45,8 @@ LET slin(gain_causality, gain, sgain, other_causality, 2,
 %% It is the CR passed to the AE3 components
 
 %DESCRIPTION four port component - effort i/o
-FOR ALL gain, input, junk, m_input, dm_input
-LET slin(gain, effort, 2, 
+FOR ALL COMPONENT, gain, input, junk, m_input, dm_input
+LET slin(COMPONENT,gain, effort, 2, 
 	input, effort, 1,
 	junk, flow, 2,
 	m_input, effort, 3,
@@ -54,8 +54,8 @@ LET slin(gain, effort, 2,
 	 = gain*dm_input*input;
 
 %DESCRIPTION four port component - flow i/o
-FOR ALL gain, input, junk, m_input, dm_input
-LET slin(gain, flow, 2, 
+FOR ALL COMPONENT, gain, input, junk, m_input, dm_input
+LET slin(COMPONENT,gain, flow, 2, 
 	input, flow, 1,
 	junk, effort, 2,
 	m_input, effort, 3,

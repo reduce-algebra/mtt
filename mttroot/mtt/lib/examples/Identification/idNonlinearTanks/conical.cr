aee4751ee83c466ff316c0d895fac0a86865e39d
Peter Gawthrop <gawthrop@users.sourceforge.net>
2003-08-18 16:40:44 +0000

    Modified for ident DIY rep (was ippp).

--- mttroot/mtt/lib/examples/Identification/idNonlinearTanks/conical.cr
+++ mttroot/mtt/lib/examples/Identification/idNonlinearTanks/conical.cr
@@ -2,8 +2,8 @@
 
 OPERATOR conical;
 %% State input.
-FOR ALL rho,g,V_0,V LET
-    conical(rho,g,V_0,effort,1,V,state,1) = rho*g*(12*(V+V_0)/pi)^(1/3);
+FOR ALL COMP, rho,g,V_0,V LET
+    conical(COMP, rho,g,V_0,effort,1,V,state,1) = rho*g*(12*(V+V_0)/pi)^(1/3);
 
 
 END;;
--- mttroot/mtt/lib/examples/Identification/idNonlinearTanks/discharge.cr
+++ mttroot/mtt/lib/examples/Identification/idNonlinearTanks/discharge.cr
@@ -3,11 +3,11 @@
 
 OPERATOR discharge;
 %% Flow input
-FOR ALL alpha,beta,F LET
-    discharge(alpha,beta,effort,1,F,flow,1) = beta*F^alpha;
+FOR ALL COMP, alpha,beta,F LET
+    discharge(COMP, alpha,beta,effort,1,F,flow,1) = beta*F^alpha;
 
 %% Effort input
-FOR ALL alpha,beta,P LET
-    discharge(alpha,beta,flow,1,P,effort,1) = (P/beta)^(1/alpha);
+FOR ALL COMP, alpha,beta,P LET
+    discharge(COMP, alpha,beta,flow,1,P,effort,1) = (P/beta)^(1/alpha);
 
 END;;

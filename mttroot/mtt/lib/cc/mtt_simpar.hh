7f5aeabe5a099888832f3eda8640f021e1857eaa
Geraint Paul Bevan <geraint@users.sourceforge.net>
2003-09-07 20:25:09 +0000

    Fixed [ 790467 ] -oct option broken

--- mttroot/mtt/lib/cc/mtt_simpar.hh
+++ mttroot/mtt/lib/cc/mtt_simpar.hh
@@ -7,14 +7,14 @@ mtt_simpar (Octave_map simpar)
 {
   static ColumnVector retval (8);
 
-  retval (0) = simpar ["first"     ].double_value ();
-  retval (1) = simpar ["last"      ].double_value ();
-  retval (2) = simpar ["dt"        ].double_value ();
-  retval (3) = simpar ["stepfactor"].double_value ();
-  retval (4) = simpar ["wmin"      ].double_value ();
-  retval (5) = simpar ["wmax"      ].double_value ();
-  retval (6) = simpar ["wsteps"    ].double_value ();
-  retval (7) = simpar ["input"     ].double_value ();
+  retval (0) = simpar ["first"     ](0).double_value ();
+  retval (1) = simpar ["last"      ](0).double_value ();
+  retval (2) = simpar ["dt"        ](0).double_value ();
+  retval (3) = simpar ["stepfactor"](0).double_value ();
+  retval (4) = simpar ["wmin"      ](0).double_value ();
+  retval (5) = simpar ["wmax"      ](0).double_value ();
+  retval (6) = simpar ["wsteps"    ](0).double_value ();
+  retval (7) = simpar ["input"     ](0).double_value ();
 
   return retval;
 }
@@ -24,14 +24,14 @@ mtt_simpar (ColumnVector simpar)
 {
   static Octave_map retval;
 
-  retval ["first"     ] = simpar (0);
-  retval ["last"      ] = simpar (1);
-  retval ["dt"        ] = simpar (2);
-  retval ["stepfactor"] = simpar (3);
-  retval ["wmin"      ] = simpar (4);
-  retval ["wmax"      ] = simpar (5);
-  retval ["wsteps"    ] = simpar (6);
-  retval ["input"     ] = simpar (7);
+  retval ["first"     ](0) = simpar (0);
+  retval ["last"      ](0) = simpar (1);
+  retval ["dt"        ](0) = simpar (2);
+  retval ["stepfactor"](0) = simpar (3);
+  retval ["wmin"      ](0) = simpar (4);
+  retval ["wmax"      ](0) = simpar (5);
+  retval ["wsteps"    ](0) = simpar (6);
+  retval ["input"     ](0) = simpar (7);
 
   return retval;
 }

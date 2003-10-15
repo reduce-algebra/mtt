96e1fba587aa2c869f54c7e9ed0b2d2a3f1f8f0a
Peter Gawthrop <gawthrop@users.sourceforge.net>
2003-10-15 16:14:38 +0000

    Correct on current value, predict next value

--- mttroot/mtt/lib/control/PPP/ppp_int_obs.m
+++ mttroot/mtt/lib/control/PPP/ppp_int_obs.m
@@ -1,4 +1,4 @@
-function [x_new,y_new,error] = ppp_int_obs (x,y,U,A,B,C,D,A_u,delta,L)
+function [x_p,y_p,error] = ppp_int_obs (x,y,U,A,B,C,D,A_u,delta,L)
 
   ## usage: x_new = ppp_int_obs (x,y,U,A,B,C,D,A_u,delta,L)
   ##
@@ -18,11 +18,11 @@ function [x_new,y_new,error] = ppp_int_obs (x,y,U,A,B,C,D,A_u,delta,L)
     L = zeros(n_x,n_y);
   endif
   
-  ## Predictor
-  [y_p,us,x_p] = ppp_ystar (A,B,C,D,x,A_u,U,delta);
+  ## Corrector (on current value of output)
+  error = (C*x-y);
+  x_new = x - L*error;
 
-  ## Corrector
-  error = (y_p-y);
-  x_new = x_p - L*error;
-  y_new = C*x_new;
+  ## Predictor (predicts Delta_OL ahead)
+  [y_p,us,x_p] = ppp_ystar (A,B,C,D,x_new,A_u,U,delta);
+
 endfunction

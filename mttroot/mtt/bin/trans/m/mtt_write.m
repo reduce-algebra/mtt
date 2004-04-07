e949e508a68d72b2d6d379715680f7d470c00779
Peter Gawthrop <gawthrop@users.sourceforge.net>
2004-04-07 12:58:48 +0000

    Now row/column independent

--- mttroot/mtt/bin/trans/m/mtt_write.m
+++ mttroot/mtt/bin/trans/m/mtt_write.m
@@ -5,6 +5,10 @@ function mtt_write(t,x,y,nx,ny,first);
 ###############################################################
 ## $Id$
 ## $Log$
+## Revision 1.6  2002/05/13 08:52:23  gawthrop
+## FIRST parameter of simpar now specifies first printed point -
+## simulation still starts at zero
+##
 ## Revision 1.5  2001/03/30 15:13:58  gawthrop
 ## Rationalised simulation modes to each return mtt_data
 ##
@@ -22,7 +26,7 @@ global MTT_data
   endif
 
   if (t>= first)
-    MTT_data = [MTT_data; t,y',t,x'];
+    MTT_data = [MTT_data; t,y(:)',t,x(:)'];
   endif
   
 endfunction

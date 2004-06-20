f37da31dab4c97248c791ea4f7f555fee7e3d059
Geraint Paul Bevan <geraint@users.sourceforge.net>
2004-06-20 21:13:53 +0000

    Changed access method for Octave_map, operator[] method no longer works.

--- mttroot/mtt/lib/cc/mtt_simpar.hh
+++ mttroot/mtt/lib/cc/mtt_simpar.hh
@@ -1,20 +1,20 @@
 
 #include <octave/oct.h>
-#include <octave/ov-struct.h>
+#include <octave/oct-map.h>
 
 static ColumnVector
 mtt_simpar (Octave_map simpar)
 {
   static ColumnVector retval (8);
 
-  retval (0) = simpar ["first"     ](0).double_value ();
-  retval (1) = simpar ["last"      ](0).double_value ();
-  retval (2) = simpar ["dt"        ](0).double_value ();
-  retval (3) = simpar ["stepfactor"](0).double_value ();
-  retval (4) = simpar ["wmin"      ](0).double_value ();
-  retval (5) = simpar ["wmax"      ](0).double_value ();
-  retval (6) = simpar ["wsteps"    ](0).double_value ();
-  retval (7) = simpar ["input"     ](0).double_value ();
+  retval (0) = simpar.contents (simpar.seek ("first"     ))(0).double_value ();
+  retval (1) = simpar.contents (simpar.seek ("last"      ))(0).double_value ();
+  retval (2) = simpar.contents (simpar.seek ("dt"        ))(0).double_value ();
+  retval (3) = simpar.contents (simpar.seek ("stepfactor"))(0).double_value ();
+  retval (4) = simpar.contents (simpar.seek ("wmin"      ))(0).double_value ();
+  retval (5) = simpar.contents (simpar.seek ("wmax"      ))(0).double_value ();
+  retval (6) = simpar.contents (simpar.seek ("wsteps"    ))(0).double_value ();
+  retval (7) = simpar.contents (simpar.seek ("input"     ))(0).double_value ();
 
   return retval;
 }
@@ -24,14 +24,14 @@ mtt_simpar (ColumnVector simpar)
 {
   static Octave_map retval;
 
-  retval ["first"     ](0) = simpar (0);
-  retval ["last"      ](0) = simpar (1);
-  retval ["dt"        ](0) = simpar (2);
-  retval ["stepfactor"](0) = simpar (3);
-  retval ["wmin"      ](0) = simpar (4);
-  retval ["wmax"      ](0) = simpar (5);
-  retval ["wsteps"    ](0) = simpar (6);
-  retval ["input"     ](0) = simpar (7);
+  retval.assign ("first"     , simpar (0));
+  retval.assign ("last"      , simpar (1));
+  retval.assign ("dt"        , simpar (2));
+  retval.assign ("stepfactor", simpar (3));
+  retval.assign ("wmin"      , simpar (4));
+  retval.assign ("wmax"      , simpar (5));
+  retval.assign ("wsteps"    , simpar (6));
+  retval.assign ("input"     , simpar (7));
 
   return retval;
 }

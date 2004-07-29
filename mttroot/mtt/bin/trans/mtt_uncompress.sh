7eda85c979f42a95c6af730812746237af3034ec
Geraint Paul Bevan <geraint@users.sourceforge.net>
2004-07-29 17:38:19 +0000

    Uncompress _abg.dia if necessary before calling dia2abg.pl

---
+++ mttroot/mtt/bin/trans/mtt_uncompress.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+file=$1
+
+gzip --test --quiet $file 2> /dev/null ; file_is_uncompressed=$?
+
+if [[ $file_is_uncompressed -eq 0 ]]; then
+    gz=`mktemp $file.gz.tmpXXXXXX`
+    mv $file $gz
+    zcat $gz > $file
+fi
+

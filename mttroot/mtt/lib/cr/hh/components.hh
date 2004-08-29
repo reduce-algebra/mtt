21e5587d1b20f2e6be64ac7e3ad3e1fcb020edb4
Geraint Paul Bevan <geraint@users.sourceforge.net>
2004-08-29 17:25:46 +0000

    Accept component types in upper or lower case.

--- mttroot/mtt/lib/cr/hh/components.hh
+++ mttroot/mtt/lib/cr/hh/components.hh
@@ -4,7 +4,8 @@
 // $ cd $(echo $MTT_COMPONENTS/simple | sed 's/\.://')
 // $ ls *_eqn.m | gawk -F_ '{printf ("%s, ", $1)}'
 enum component {
-  AE, AF, C, EBTF, EMTF, ES, FMR, FP, GY, I, PS, RST, RS, R, SS, TF
+  AE, AF, C, EBTF, EMTF, ES, FMR, FP, GY, I, PS, RST, RS, R, SS, TF,
+  ae, af, c, ebtf, emtf, es, fmr, fp, gy, i, ps, rst, rs, r, ss, tf
 };
 
 typedef enum component component_t;

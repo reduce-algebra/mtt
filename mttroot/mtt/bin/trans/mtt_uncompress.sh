f6f6ae65cbd78eb2a1781f26a2365ec999c0303a
Geraint Paul Bevan <geraint@users.sourceforge.net>
2005-02-17 18:36:38 +0000

    Removed a bash-ism: replaced [[ conditional ]] with [ conditional ]

--- mttroot/mtt/bin/trans/mtt_uncompress.sh
+++ mttroot/mtt/bin/trans/mtt_uncompress.sh
@@ -4,7 +4,7 @@ file=$1
 
 gzip --test --quiet $file 2> /dev/null ; file_is_uncompressed=$?
 
-if [[ $file_is_uncompressed -eq 0 ]]; then
+if [ $file_is_uncompressed -eq 0 ]; then
     gz=`mktemp $file.gz.tmpXXXXXX`
     mv $file $gz
     zcat $gz > $file
